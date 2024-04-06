18030740ccb0ce253773ed2c20f7ae910cce824d
Nicolás Abramovich <104592905+abranico@users.noreply.github.com>
2024-04-06 20:24:47 -0300

    Add files via upload

---
+++ insert_data.sh
@@ -0,0 +1,58 @@
+#! /bin/bash
+
+if [[ $1 == "test" ]]
+then
+  PSQL="psql --username=postgres --dbname=worldcuptest -t --no-align -c"
+else
+  PSQL="psql --username=freecodecamp --dbname=worldcup -t --no-align -c"
+fi
+
+# Do not change code above this line. Use the PSQL variable above to query your database.
+
+echo $($PSQL "TRUNCATE TABLE games, teams")
+
+cat games.csv | while IFS=',' read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+do
+
+  if [[ $WINNER != winner ]]
+  then
+    TEAM_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+    if [[ -z $TEAM_ID ]]
+    then
+        INSERT_TEAM_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
+        if [[ $INSERT_TEAM_RESULT == "INSERT 0 1" ]]
+        then
+            echo "Inserted into teams, $WINNER"
+        fi
+    fi
+  fi
+
+  if [[ $OPPONENT != opponent ]]
+  then
+    TEAM_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+    if [[ -z $TEAM_ID ]]
+    then
+        INSERT_TEAM_RESULT=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
+        if [[ $INSERT_TEAM_RESULT == "INSERT 0 1" ]]
+        then
+            echo "Inserted into teams, $OPPONENT"
+        fi
+    fi
+  fi
+
+  if [[ $YEAR != year ]]
+  then
+    WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER' ")
+    OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT' ")
+    GAME_ID=$($PSQL "SELECT game_id FROM games WHERE winner_id=$WINNER_ID AND opponent_id=$OPPONENT_ID")
+    if [[ -z $GAME_ID ]]
+    then
+      INSERT_GAME_RESULT=$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)" )
+      if [[ $INSERT_GAME_RESULT == "INSERT 0 1" ]]
+      then
+        echo "Inserted into games, $YEAR - $WINNER vs $OPPONENT"
+      fi
+    fi
+  fi
+  
+done
